1f0addceefc1db7506ee52108bf0bc747c7d0bff
karimzidan007 <karimzidan7474@gmail.com>
2024-07-05 22:49:32 +0300

    reset

--- LINUXADMIN_LAB1/lab1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir ~/myDirectory
-mkdir ~/myDirectory/secondDirectory
-touch ~/myDirectory/secondDirectory/myNotePaper
-cp ~/myDirectory/secondDirectory/myNotePaper ~/myDirectory
-mv ~/myDirectory/myNotePaper  ~/myDirectory/myOldNotePaper
